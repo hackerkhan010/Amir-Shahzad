925cc174ff6f1af629e075c2ca5e3d8b72b52e1b
hackerkhan010 <107766834+hackerkhan010@users.noreply.github.com>
2022-06-18 14:02:09 -0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -2,7 +2,8 @@ username=input("Enter Your Username :")
 password=input("Enter Your Password :")
 if username == 'admin':
   if password =='admin':
-    print("Login Successful")
-    if username !='admin':
-    if password !='admin':
-      print("Incorrect Please Try Again")
+      print("Login Successful")
+  else:
+      print("Incorrect Password")
+else:
+      print("Incorrect Username")
