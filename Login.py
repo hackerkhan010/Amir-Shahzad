1ab590eb256574dffdd30daaa714aa05c9e2b923
hackerkhan010 <107766834+hackerkhan010@users.noreply.github.com>
2022-06-18 13:46:29 -0700

    Create Login.py

---
+++ Login.py
@@ -0,0 +1,8 @@
+username=input("Enter Your Username :")
+password=input("Enter Your Password :")
+if username == 'admin':
+  if password =='admin':
+    print("Login Successful")
+    if username !='admin':
+    if password !='admin':
+      print("Incorrect Please Try Again")
